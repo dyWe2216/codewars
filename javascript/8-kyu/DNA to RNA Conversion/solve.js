959bcec40427f3ae84b156c61457e261bb94fff0
dyWe2216 <dnleodud@gmail.com>
2022-04-07 23:47:50 +0900

    add: Done solving the two problems
    - 8-kyu/DNA to RNA Conversion/solve.js
    - 8-kyu/Your function takes two arguments:/solve.js

---
+++ javascript/8-kyu/DNA to RNA Conversion/solve.js
@@ -0,0 +1,8 @@
+// const DNAtoRNA = dna => dna.split('').map(word => word === 'T' ? 'U' : word).join('')
+
+// Batter Then
+// const DNAtoRNA = dna => dna.replace(/T/g, 'U');
+
+console.log(DNAtoRNA("TTTT"));
+console.log(DNAtoRNA("GCAT"));
+console.log(DNAtoRNA("GACCGCCGCC"));
---
+++ javascript/8-kyu/Your function takes two arguments:/solve.js
@@ -0,0 +1,7 @@
+const twiceAsOld = (dad, son) => Math.abs(dad - (son * 2));
+
+console.log(twiceAsOld(36,7))
+console.log(twiceAsOld(55,30))
+console.log(twiceAsOld(42,21))
+console.log(twiceAsOld(22,1))
+console.log(twiceAsOld(29,0))
